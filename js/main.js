a328845701e85f5bce49b0707a5aef48067781ef
miakotalik <mia@rmtwentynine.com>
2020-08-19 18:57:28 -0700

    first commit

--- js/main.js
+++ js/main.js
@@ -23,17 +23,119 @@ setTimeout(function () {
 // Strongly recommended: Hide loader after 20 seconds, even if the page hasn't finished loading
 setTimeout(hideLoader, 20 * 1000);
 
-// ========================================================================= //
-//  //POP OVER
-// ========================================================================= //
+jQuery_3_5_1(document).ready(function () {
+  jQuery_3_5_1("#flip").click(function () {
+
+    if (jQuery_3_5_1("#contactPanel").is(':visible')) {
+      jQuery_3_5_1("#contactPanel").fadeToggle("slow");
+      jQuery_3_5_1("#aboutPanel").fadeIn("slow");
+    } else {
+      jQuery_3_5_1("#aboutPanel").slideDown("slow");
+    }
+
+    if (jQuery_3_5_1("#buyPanel").is(':visible')) {
+      jQuery_3_5_1("#buyPanel").fadeToggle("slow");
+      jQuery_3_5_1("#aboutPanel").fadeIn("slow");
+    } else {
+      jQuery_3_5_1("#aboutPanel").slideDown("slow");
+    }
+
+    if (jQuery_3_5_1(" #bookPanel").is(':visible')) {
+      jQuery_3_5_1("#bookPanel").fadeToggle("slow");
+      jQuery_3_5_1("#aboutPanel").fadeIn("slow");
+    } else {
+      jQuery_3_5_1("#aboutPanel").slideDown("slow");
+    }
+
+  });
+});
 
 
-var popOverSettings = {
-  trigger: 'hover',
-  container: 'body',
-  html: true,
-  selector: '[data-toggle="popover"]',
+jQuery_3_5_1(document).ready(function () {
+  jQuery_3_5_1("#contactFlip").click(function () {
 
-}
+    if (jQuery_3_5_1("#aboutPanel").is(':visible')) {
+      jQuery_3_5_1("#aboutPanel").fadeToggle("slow");
+      jQuery_3_5_1("#contactPanel").fadeIn("slow");
+    } else {
+      jQuery_3_5_1("#contactPanel").slideDown("slow");
+    }
 
-$('body').popover(popOverSettings);
+    if (jQuery_3_5_1("#buyPanel").is(':visible')) {
+      jQuery_3_5_1("#buyPanel").fadeToggle("slow");
+      jQuery_3_5_1("#contactPanel").fadeIn("slow");
+    } else {
+      jQuery_3_5_1("#contactPanel").slideDown("slow");
+    }
+
+    if (jQuery_3_5_1(" #bookPanel").is(':visible')) {
+      jQuery_3_5_1("#bookPanel").fadeToggle("slow");
+      jQuery_3_5_1("#contactPanel").fadeIn("slow");
+    } else {
+      jQuery_3_5_1("#contactPanel").slideDown("slow");
+    }
+
+  });
+});
+
+jQuery_3_5_1(document).ready(function () {
+  jQuery_3_5_1("#buyFlip").click(function () {
+
+    if (jQuery_3_5_1("#aboutPanel").is(':visible')) {
+      jQuery_3_5_1("#aboutPanel").fadeToggle("slow");
+      jQuery_3_5_1("#buyPanel").fadeIn("slow");
+    } else {
+      jQuery_3_5_1("#buyPanel").slideDown("slow");
+    }
+
+    if (jQuery_3_5_1("#contactPanel").is(':visible')) {
+      jQuery_3_5_1("#contactPanel").fadeToggle("slow");
+      jQuery_3_5_1("#buyPanel").fadeIn("slow");
+    } else {
+      jQuery_3_5_1("#buyPanel").slideDown("slow");
+    }
+
+    if (jQuery_3_5_1(" #bookPanel").is(':visible')) {
+      jQuery_3_5_1("#bookPanel").fadeToggle("slow");
+      jQuery_3_5_1("#buyPanel").fadeIn("slow");
+    } else {
+      jQuery_3_5_1("#buyPanel").slideDown("slow");
+    }
+
+  });
+});
+
+jQuery_3_5_1(document).ready(function () {
+  jQuery_3_5_1("#bookFlip").click(function () {
+
+    if (jQuery_3_5_1("#aboutPanel").is(':visible')) {
+      jQuery_3_5_1("#aboutPanel").fadeToggle("slow");
+      jQuery_3_5_1("#bookPanel").fadeIn("slow");
+    } else {
+      jQuery_3_5_1("#bookPanel").slideDown("slow");
+    }
+
+    if (jQuery_3_5_1("#contactPanel").is(':visible')) {
+      jQuery_3_5_1("#contactPanel").fadeToggle("slow");
+      jQuery_3_5_1("#bookPanel").fadeIn("slow");
+    } else {
+      jQuery_3_5_1("#bookPanel").slideDown("slow");
+    }
+
+    if (jQuery_3_5_1(" #buyPanel").is(':visible')) {
+      jQuery_3_5_1("#buyPanel").fadeToggle("slow");
+      jQuery_3_5_1("#bookPanel").fadeIn("slow");
+    } else {
+      jQuery_3_5_1("#bookPanel").slideDown("slow");
+    }
+
+  });
+});
+
+
+
+
+$(".main-nav a").click(function () {
+  $(".main-nav a").css("color", "#000000");
+  $(this).css("color", "#e24b20");
+});
